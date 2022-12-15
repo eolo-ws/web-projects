0bd53da44a5ec9ea1f8d20d84c353f61d2020132
eolo-ws <benjamincorderog@gmail.com>
2022-12-15 15:11:22 -0300

    Revert "jwt in user-app and jwt reception and cookie hdlng"
    This reverts commit d6251e9dca732b46d8e2f025a66985d748c32861.

--- flask-angular-mysql/flask/user-app/app.py
+++ flask-angular-mysql/flask/user-app/app.py
@@ -1,13 +1,9 @@
 from flask import Flask, request, jsonify
 import mysql.connector
 from flask_cors import CORS
-import jwt
-import crypto
-import datetime
 
-
-private_key = crypto.generate_private_key(crypto.TYPE_RSA, 2048)
-public_key = crypto.dump_publickey(crypto.FILETYPE_PEM, private_key)
+app = Flask(__name__)
+CORS(app,support_credentials=True)
 
 config = {
     'user': 'root',
@@ -17,49 +13,32 @@ config = {
     'database': 'tasks'
 }
 
-
-app = Flask(__name__)
-CORS(app, support_credentials=True)
-
-
 @app.route('/auth', methods=['POST'])
 def auth():
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
+    # if not request.is_json:
+    #     return jsonify({"msg": "Missing JSON in request"}), 400
 
     username = request.json.get('username', None)
     password = request.json.get('password', None)
+    # if not username:
+    #     return jsonify({"msg": "Missing username parameter"}), 400
+    # if not password:
+    #     return jsonify({"msg": "Missing password parameter"}), 400
+    
+    # Check if the username and password are correct
+    # conn = mysql.connector.connect(**mysql_config)
+    # cur = conn.cursor()
+    # cur.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+    # user = cur.fetchone()
+    # cur.close()
+    # conn.close()
 
-    if not username or not password:
-        return jsonify({"msg": "Missing username or password parameter"}), 400
 
-    # Check the username and password against the database
-    mydb = mysql.connector.connect(
-        host=config['host'],
-        user=config['user'],
-        passwd=config['password'],
-        database=config['database'],
-        port=config['port']
-    )
+    return jsonify({'username':username,'password':password})
 
-    mycursor = mydb.cursor()
-    mycursor.execute(
-        "SELECT * FROM users WHERE username = %s and password = %s", (username, password))
-    users = mycursor.fetchall()
-    if len(users) == 0:
-        return jsonify({"msg": "Invalid username or password"}), 401
-
-    # Generate a JWT token and return it to the client
-    user = users[0]
-    expiration_time = datetime.datetime.now() + datetime.timedelta(weeks=12)
-    payload = {
-        "username": user[0],
-        "password": user[1],
-        "exp": expiration_time
-    }
-    token = jwt.encode(payload, private_key, algorithm="RS256")
-    return jsonify({'token': token.decode('utf-8'), 'public_key': public_key})
-
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
